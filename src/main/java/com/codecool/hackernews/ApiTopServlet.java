99ba50f5423be7bed89dce324db6ff908131412f
Kattkovacs <kattkovacs@gmail.com>
2020-08-04 15:51:42 +0200

    refactored ApiTopServlet

--- src/main/java/com/codecool/hackernews/ApiTopServlet.java
+++ src/main/java/com/codecool/hackernews/ApiTopServlet.java
@@ -1,12 +1,8 @@
 package com.codecool.hackernews;
 
-import com.codecool.hackernews.modal.NewsModal;
-import com.codecool.hackernews.modal.PageModal;
-import com.codecool.hackernews.modal.ResponseNewsModal;
+import com.codecool.hackernews.models.NewsModel;
+import com.codecool.hackernews.models.ResponseNewsModel;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,58 +14,48 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 @WebServlet(name = "apiTopServlet", urlPatterns = {"/api/top"}, loadOnStartup = 4)
 public class ApiTopServlet extends javax.servlet.http.HttpServlet {
-    private Gson gson = new Gson();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String page = req.getParameter("page");
+        String content = getPageContent(req.getParameter("page"));
+        NewsModel[] newsModels = new Gson().fromJson(content, NewsModel[].class);
+        ResponseNewsModel[] responseNewsModels = extractNewsModel(newsModels);
+        String responseString = new Gson().toJson(responseNewsModels);
+
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        out.print(responseString);
+        out.flush();
+    }
+
+    private ResponseNewsModel[] extractNewsModel(NewsModel[] newsModels) {
+        ResponseNewsModel[] responseNewsModels = new ResponseNewsModel[newsModels.length];
+        for (int i = 0; i < newsModels.length; i++) {
+            responseNewsModels[i] = new ResponseNewsModel(newsModels[i]);
+        }
+        return responseNewsModels;
+    }
+
+    private String getPageContent(String page) throws IOException {
         if (page == null || "0".equals(page)) page = "1";
 
         URL url = new URL("https://api.hnpwa.com/v0/news/" + page + ".json");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
 
-        int status = con.getResponseCode();
-
-        Scanner sc = new Scanner(url.openStream());
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
         StringBuilder content = new StringBuilder();
-        while (sc.hasNext()) {
-            content.append(sc.nextLine());
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
         }
-        System.out.println(content);
-        sc.close();
+        in.close();
         con.disconnect();
-
-        JsonParser parse = new JsonParser();
-        JsonArray jsonArray = (JsonArray) parse.parse(content.toString());
-        PageModal pageModal = new PageModal();
-        for (var object : jsonArray) {
-            pageModal.addNews(new NewsModal(
-                    ((JsonObject) object).get("id"),
-                    ((JsonObject) object).get("points"),
-                    ((JsonObject) object).get("title"),
-                    ((JsonObject) object).get("time_ago"),
-                    ((JsonObject) object).get("author"),
-                    ((JsonObject) object).get("url"),
-                    ((JsonObject) object).get("type"),
-                    ((JsonObject) object).get("domain")));
-        }
-        ResponseNewsModal[] responseNewsModals = new ResponseNewsModal[pageModal.getNews().size()];
-        for (int i = 0; i < responseNewsModals.length ; i++) {
-            responseNewsModals[i] = new ResponseNewsModal(pageModal.getNews().get(i));
-        }
-        String response = gson.toJson(responseNewsModals);
-
-        PrintWriter out = resp.getWriter();
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        out.print(response);
-        out.flush();
+        return content.toString();
     }
 }
-
--- src/main/java/com/codecool/hackernews/HackerNewServlet.java
+++ src/main/java/com/codecool/hackernews/HackerNewServlet.java
@@ -6,7 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "hackerNewsServlet", urlPatterns = {"/"}, loadOnStartup = 1)
 public class HackerNewServlet extends javax.servlet.http.HttpServlet {
@@ -18,41 +19,14 @@ public class HackerNewServlet extends javax.servlet.http.HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         String title = "Michael Hackson news";
-
-        String navBar = "<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\n" +
-                "  <a class=\"navbar-brand\" href=\"\\\">Hackson news</a>\n" +
-                "  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavAltMarkup\" aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
-                "    <span class=\"navbar-toggler-icon\"></span>\n" +
-                "  </button>\n" +
-                "  <div class=\"collapse navbar-collapse\" id=\"navbarNavAltMarkup\">\n" +
-                "    <div class=\"navbar-nav\">\n" +
-                "      <a class=\"nav-item nav-link active\" href=\"#\">Top news <span class=\"sr-only\">(current)</span></a>\n" +
-                "      <a class=\"nav-item nav-link\" href=\"#\">Newest</a>\n" +
-                "      <a class=\"nav-item nav-link\" href=\"#\">Jobs</a>\n" +
-                "    </div>\n" +
-                "  </div>\n" +
-                "</nav>";
-
-        String footer = "<!-- Footer -->\n" +
-                "<footer class=\"page-footer font-small blue\">\n" +
-                "\n" +
-                "  <div class=\"footer-copyright text-center py-3\">© 2020 Copyright: Panka\n" +
-                "    <a href=\"https://mdbootstrap.com/\"> mervay.panka@gmail.com</a>\n" +
-                "  </div>\n" +
-                "\n" +
-                "</footer>\n";
+        List<String> headItems = new ArrayList<>();
+        headItems.add("bootstrap");
 
         out.println(
                 "<html>\n" +
-                        "<head>" +
-                        "  <title>" + title + "</title>" +
-                        "  <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>" +
-                        "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css\">" +
-                        "  <link rel=\"stylesheet\" type=\"text/css\" href='/static/css/site.css' />" +
-                        "  <script type=\"module\" src=\"/static/js/main.js\" defer> </script>" +
-                        "</head>\n" +
+                        StaticHtmlParts.getHead(title, headItems) +
                         "<body>\n" +
-                        navBar +
+                        StaticHtmlParts.getMenubar() +
                         "<h1 align = \"center\">" + title + "</h1>\n" +
                         "<ul>\n" +
                         "<li><b>First Name</b>: " + request.getParameter("first_name") + "\n" +
@@ -61,9 +35,8 @@ public class HackerNewServlet extends javax.servlet.http.HttpServlet {
                         "<div class='visit'>You can serve any static content from <span class='folder'>webapp/static</span> folder, like a css file.</div>" +
                         "<div>Visit another servlet: <a href=\"/another\">Visit the other servlet</a></div>" +
                         "<div>You can provide a json file as well: <a href=\"/json\">Visit Hacker News json data example</a></div>" +
-                        "</body> " +
-                         footer +
-                        "</html>"
+                        StaticHtmlParts.getFooter() +
+                        "</body></html>"
         );
     }
 }
